a31d48ff4ed9465927754d2d9f4d8f52bad2e68e
mukeshvijayakumar <mukesh@techasoft.com>
2024-11-19 15:26:37 +0530

    explored validaor library

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+const validator = require("validator");
+
 // schema validation
 const userSchema = new mongoose.Schema(
   {
@@ -16,12 +18,22 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       lowercase: true,
-      unique: true,
+      unique: [true, "email must be unique"],
       trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error(`Invalid Email Addres! ${value}`);
+        }
+      },
     },
     password: {
       type: String,
       required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error(`Please put starong password ${value}`);
+        }
+      },
     },
     age: {
       type: Number,
@@ -39,6 +51,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       default:
         "https://static.vecteezy.com/system/resources/previews/035/624/129/non_2x/user-profile-person-icon-in-flat-isolated-in-suitable-for-social-media-man-profiles-screensavers-depicting-male-face-silhouettes-for-apps-website-vector.jpg",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error(`Invalid Image URL ${value}`);
+        }
+      },
     },
     about: {
       type: String,
