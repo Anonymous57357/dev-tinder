86bf321980a6cedc64c4f5ed24b77c6cb571feab
mukeshvijayakumar <mukesh@techasoft.com>
2025-01-21 15:15:21 +0530

    fix

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -34,7 +34,6 @@ authRouter.post("/signup", async (req, res) => {
     const cookieOptions = {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // Use secure cookies in production
-      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
       path: "/", // Make cookie accessible across the app
     };
@@ -71,7 +70,6 @@ authRouter.post("/login", async (req, res) => {
       const cookieOptions = {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production", // Use secure cookies in production
-        sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
         path: "/", // Make cookie accessible across the app
       };
